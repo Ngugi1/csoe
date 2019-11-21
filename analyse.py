2e57c870a7f3fe28d831aa9829d9c62e0f455682
Sam Ngugi😀 <ngugindunguapps@gmail.com>
2019-11-21 01:48:00 +0100

    Fix number of devs bug

--- analyse.py
+++ analyse.py
@@ -11,11 +11,7 @@ import re
 java_file_regex = re.compile("")
 
 # Repositories and their main branches
-repos = [("aries", "trunk"), 
-                ("falcon", "master"), 
-                ("ranger","master"),
-                ("sqoop", "trunk"),
-                ("whirr", "trunk")]
+repos = [("aries", "trunk")]
 
 # Prepare repo for mining
 def configRepo(repo, reversed=None, 
@@ -76,45 +72,59 @@ def getFileName(modification):
 # Given a unix timestamp, give back a date
 def unixTimestampToDate(timestamp):
     return datetime.fromtimestamp(timestamp)
-def updateNumberOfDevs(dictionary, key):
-      # No. of developers is at index 3
+def updateNumberOfDevs(dictionary, key, developer):
+    # No. of developers is at index 3
     if keyExists(dictionary,key):
-        return dictionary[key][3] + 1
+        dictionary[key][3].add(developer)
+        return dictionary[key][3]
     else:
-        return 1
+        return {developer}
 #  Update number of changes 
-def updateNumberOfChanges(dictionary, key):
+def updateNumberOfFileChanges(dictionary, key):
     # Number of changes  is at index 2 
     if keyExists(dictionary, key):
         return dictionary[key][2] + 1
     else:
         return 0
+
 #  does a key exist in th e dictionary?
 def keyExists(dictionary, key):
-    key in dictionary
+    if key in dictionary:
+        return True
+    else:
+        return False
 # Calculate all metrics for a given time period
 def processTimePeriod(period):
     # Period : (start_period, end_period)
     start,end = period
     # Metrics is a dictionary to keep track of metrics for this period
     metrics = {}
+    # total changes over this period
+    total_changes = 0
+    # Initially, total changes is 0 
     # Traverse all commits in the time period
     for commit in traverse(configRepo(repo, start=unixTimestampToDate(start), end=unixTimestampToDate(end))):
         # Find all modifications
         for modification in commit.modifications:
-            # Uopdate the metrics
+            # Update the metrics
             if(getFileName(modification).endswith('.java')):
+                total_changes += 1
                 metrics[getFileName(modification)] = [getLinesOfCode(modification), 
-                                                        getComplexity(modification),
-                                                        # If the file is not added already, then this is the first time it was added
-                                                    updateNumberOfChanges(metrics,getFileName(modification)),
-                                                    updateNumberOfDevs(metrics, getFileName(modification))]
+                                                      getComplexity(modification),
+                                                      updateNumberOfFileChanges(metrics,getFileName(modification)),
+                                                      updateNumberOfDevs(metrics,getFileName(modification), commit.author.email),
+                                                      total_changes]
+    #  Compute entropy of changes - probability that the file will change
+    for filename in metrics:
+        # Add entropy for every file
+        metrics[filename][3] = len(metrics[filename][3])
+        print(metrics[filename][3])
+        metrics[filename][4] = float(format(metrics[filename][2] / total_changes, ".6f"))
     print(metrics)
-
-
 # Process all repos
 for repo in repos:
     start, end = getRepoLife(configRepo(repo), configRepo(repo, reversed=True))
     for period in splitTimePeriods(int(start.strftime("%s")),int(end.strftime("%s")),3):
+        # Basic metrics for a period
         processTimePeriod(period)
 
